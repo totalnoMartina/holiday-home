9e206d388661673de860fae7890c1786bf9cb700
totalnoMartina <totalnobzvz@yahoo.com>
2022-04-04 22:49:36 +0000

    Created model of booking function, add bookings

--- holidayapp/models.py
+++ holidayapp/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+APARTMENTS = (('Tony', 'Tony Apartment for max4 people'),
+                ('Matea', 'Matea apartment for max4 people'),
+                ('Martina', 'Martina apartment for max6 people'))
+
+class Apartment(models.Model):
+    """ A model for apartment """
+    apartment_name = models.CharField(choices=APARTMENTS, max_length=10, primary_key=True)
+    beds_nr = models.IntegerField()
+    guest_nr = models.IntegerField()
+    description = models.TextField(name='description', null=True)
+    image1 = models.ImageField(blank=True)
+    image2 = models.ImageField(blank=True)
+    image3 = models.ImageField(blank=True)
+    image4 = models.ImageField(blank=True)
+    image5 = models.ImageField(blank=True)
+    image6 = models.ImageField(blank=True)
+    image7 = models.ImageField(blank=True)
+    image8 = models.ImageField(blank=True)
+
+    def __str__(self):
+            """ Showing apartment class model created """
+            return f'An apartment {self.apartment_name} with {self.beds_nr} beds for {self.guest_nr} is created'
+
+
+class Booking(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE)
+    check_in = models.DateField()
+    check_out = models.DateField()
+
+    def __str__(self):
+        return f'A {self.user} has booked {self.apartment} from {self.check_in} until {self.check_out}'
+    
